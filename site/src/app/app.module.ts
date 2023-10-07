f4e4b3981d9fbf1e3acbddae8aab36b50f6b48e5
Cleber <cleberdeoliveira.ti@gmail.com>
2023-10-06 21:35:44 -0300

    aula 1

---
+++ site/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddressComponent } from './components/address/address.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    FeaturesComponent,
+    AboutComponent,
+    AddressComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
